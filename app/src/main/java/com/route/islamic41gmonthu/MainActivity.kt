950364690a24f1164ac42cc27f0eafbdd7b035b2
Abdallah El-Zoka <abdallah.elzoka@gmail.com>
2025-01-16 20:25:10 +0200

    Initial Commit

---
+++ app/src/main/java/com/route/islamic41gmonthu/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.route.islamic41gmonthu
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    // 1- Version Control Systems (Git)
+    // 2- Islamic App
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    // 1- Enable Version Control Integration to this project
+
+    //      Repository -> Commits  (States) Code Changes
+    //
+    // Git File States :-
+    // 1- UnVersioned Files
+    // 2- Versioned Files
+    // 3- Ignored Files
+
+    // Git -> Add Files to VCS
+    // .gitignore
+
+}
